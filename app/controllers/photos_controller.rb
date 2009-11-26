ad3f01f15304be851cfe7f37228a8e7aab0ddadc
Ellis Berner <bernee@rpi.edu>
2009-11-26 03:36:49 -0500

    Email

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,3 @@
 class PhotosController < ApplicationController
-  def destroy
-    @photo = photo.find(params[:id])
-    @photo.destroy
-    user = @photo.attachable
-    @allowed = 5 - user.photos.count
-  end
+ 
 end
