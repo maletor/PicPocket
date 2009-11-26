ee3551450eb3c84de09339371b6d7bdefd358e9e
Ellis Berner <bernee@rpi.edu>
2009-11-26 02:23:51 -0500

    Modified email code

--- app/models/incoming_mail_handler.rb
+++ app/models/incoming_mail_handler.rb
@@ -15,11 +15,13 @@ class IncomingMailHandler < ActionMailer::Base
     # Remember to make UI option that users can opt out of registration 
     # and either not send emails or send them to a username+32523@picpocket.com
     # type address.
-    if (@user = User.find_by_email(email.from))
+    if (user = User.find_by_email(email.from))
       mms.media.each do |key, value|
         if key.include?('image')
-          @photo = Photo.new(:uploaded_data => value, :title => email.subject.empty? ? "Untitled" : email.subject)
-          @user.photos << @photo
+          value.each do |file| 
+            photo = Photo.new(:uploaded_data => File.open(file), :title => email.subject.empty? ? "Untitled" : email.subject)
+            user.photos << photo
+          end
         end
       end
     else
