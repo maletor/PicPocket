6dffebc6dfdf8a321c5c489f23cb29a5eb6fa060
Ellis Berner <bernee@rpi.edu>
2009-11-26 11:35:26 -0500

    Working on email

--- app/models/incoming_mail_handler.rb
+++ app/models/incoming_mail_handler.rb
@@ -6,29 +6,32 @@ class IncomingMailHandler < ActionMailer::Base
   # Receives email(s) from MMS-Email or regular email and 
   # uploads that content the user's photos.
   # TODO: Use beanstalkd for background queueing and processing.
+  # TODO: Remember to get SpamAssasin
   def receive(email)    
-    
-    mms = MMS2R::Media.new(email)
-    
-    ## 
-    # Ok to find user by email as long as activate upon registration.
-    # Remember to make UI option that users can opt out of registration 
-    # and either not send emails or send them to a username+32523@picpocket.com
-    # type address.
-    ##
-    # Remember to get SpamAssasin
-    if (@user = User.find_by_email(email.from) && email.has_attachments?)
-      mms.media.each do |key, value|
-        if key.include?('image')
-          value.each do |file| 
-            @user.photos.push Photo.create!(:uploaded_data => File.open(file), :title => email.subject.empty? ? "Untitled" : email.subject)
+    begin
+      mms = MMS2R::Media.new(email)
+      
+      ## 
+      # Ok to find user by email as long as activate upon registration.
+      # Remember to make UI option that users can opt out of registration 
+      # and either not send emails or send them to a username+32523@picpocket.com
+      # type addresss
+      user = User.find_by_email(email.from)
+      
+      if (user && email.has_attachments?)
+        mms.process do |media_type, files|
+          for file in files
+            user.photos.push Photo.create!(:uploaded_data => file, :title => "From mail") if media_type =~ /image/
           end
         end
       end
-    end   
-    
-    mms.purge
-    
+      
+    ensure
+      
+      mms.purge
+      
+    end
+        
   end
 
 end
