728d435668d7b833f183ae66576bef8e549b053f
Ellis Berner <bernee@rpi.edu>
2009-11-29 03:03:09 -0500

    Stuff

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,14 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :photos
   
-  map.info    ':permalink', :controller => 'pages', :action => 'show'
   map.signup    'signup', :controller => 'users', :action => 'new'
   map.logout    'logout', :controller => 'sessions', :action => 'destroy'
   map.login     'login', :controller => 'sessions', :action => 'new'
   map.activate  'activate/:activation_code', :controller => 'users', :action => 'activate'
   map.forgot    'forgot', :controller => 'users', :action => 'forgot'
   map.reset     'reset/:reset_code', :controller => 'users', :action => 'reset'
+  map.info    ':permalink', :controller => 'pages', :action => 'show'
+  
       
   # The priority is based upon order of creation: first created -> highest priority.
 
