8aab1013dc5981f2ad2fa69a63495bf79f14e61a
Ellis Berner <bernee@rpi.edu>
2009-11-29 02:25:31 -0500

    Testing mailers

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,6 +1,4 @@
 require 'mms2r'
-# require 'action_controller'
-# require 'action_controller/test_process.rb'
 
 class UserMailer < ActionMailer::Base
   
@@ -25,65 +23,39 @@ class UserMailer < ActionMailer::Base
   end
   
   protected
-    def setup_email(user)
-      @recipients  = "#{user.email}"
-      @from        = "no-reply@spounce.com"
-      @subject     = "[beta.spounce.com] "
-      @sent_on     = Time.now
-      @body[:user] = user
-    end
+  
+  def setup_email(user)
+    @recipients  = "#{user.email}"
+    @from        = "no-reply@spounce.com"
+    @subject     = "Spounce | "
+    @sent_on     = Time.now
+    @body[:user] = user
+  end
 
   ##
-  # Receives email(s) from MMS-Email or regular email and 
-  # uploads that content the user's photos.
-  # TODO Use beanstalkd for background queueing and processing.
+  # Receives email (only one) from MMS-Email or regular email and 
+  # uploads that content to the user's photos.
+  # TODO Use background queueing and processing.
   # TODO Remember to get SpamAssasin for production
-  # TODO Maybe see if thoughtbot's paperclip could provide something a little cleaner
-  # TODO This should be done with BackgroundRB
   def receive(email)    
     begin
       mms = MMS2R::Media.new(email)
       
-      ## 
-      # Ok to find user by email as long as activate upon registration.
-      # Remember to make UI option that users can opt out of registration 
-      # and either not send emails or send them to a username+32523@spounce.com
-      # type addresss
-      
       if user = User.find_by_email(mms.from)
-        mms.media.each do |key, files|
-          if key.include?('image')
-            files.each do |path|
-              mimetype = key
-              
-              photo = Photo.new(:uploaded_data => ActionController::TestUploadedFile.new(path, mimetype))
-              photo.save
-              user.photos << photo 
-              
-              # file = File.new(path)
-              #               
-              #               
-              # def file.local_path
-              #   self.path
-              # end
-              # 
-              # def file.original_filename
-              #   File.basename(self.path)
-              # end
-              # 
-              # def file.size
-              #   File.size(self.path)
-              # end
-              # 
-              # class << file
-              #   self
-              # end.send(:define_method, :content_type) { mime_type }
-              #               
-              # user.photos << Photo.create(:uploaded_data => file)
-              
-            end
-          end
+        if mms.default_media.content_type.includes?('image')
+          user.photos << Photo.create!(:uploaded_data => mms.default_media) 
+        ##
+        # Video model does not yet exist
+        # elsif mms.default_media.content_type.includes?('video')
+         # user.videos << Video.create!(:uploaded_data => mms.default_media)
+        else
+          ##
+          # Error reporting for user as well?
+          logger.info("This is not a recognized filetype.")
         end
+          
+      else
+        logger.info("No user found as #{mms.from}.")
       end
       
       
